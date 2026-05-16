7f95ddc64e612b3de01e1ddade23735d6cda04c7
AquillaSG <aquillagomes@hotmail.com>
2026-05-15 23:01:53 -0300

    translated to english for better communication and standard

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,16 +1,16 @@
 public class BinarySearch {
     // Efficiency: O(log n)
-    public static int search(int[] array, int alvo) {
+    public static int search(int[] array, int target) {
         int low = 0;
         int high = array.length - 1;
 
         while (low <= high) {
             int middle = low + (high - low) / 2;
 
-            if (array[middle] == alvo) {
+            if (array[middle] == target) {
                 return middle;
             }
-            if (array[middle] > alvo) {
+            if (array[middle] > target) {
                 high = middle - 1;
             } else {
                 low = middle + 1;
